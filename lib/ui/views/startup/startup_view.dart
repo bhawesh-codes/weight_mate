5b0a079d6041234efbfb495d0cde3ee9bb9a56b3
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-07-04 17:11:18 +0545

    feat: create start view and viewmodel

--- lib/ui/views/startup/startup_view.dart
+++ lib/ui/views/startup/startup_view.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:flutter_screenutil_plus/flutter_screenutil_plus.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:stacked/stacked.dart';
+import 'package:weight_mate/base/utils/app_assets.dart';
+import 'package:weight_mate/base/utils/text_type.dart';
+import 'package:weight_mate/base/utils/ui_helper.dart';
 import 'package:weight_mate/base/widgets/scaffold/base_app_scaffold.dart';
+import 'package:weight_mate/base/widgets/text/body_text.dart';
+import 'package:weight_mate/base/widgets/text/title_text.dart';
 import 'package:weight_mate/ui/common/app_colors.dart';
-import 'package:weight_mate/ui/common/ui_helpers.dart';
 
 import 'startup_viewmodel.dart';
 
@@ -16,31 +22,31 @@ class StartupView extends StackedView<StartupViewModel> {
     StartupViewModel viewModel,
     Widget? child,
   ) {
-    return const BaseAppScaffold(
+    return BaseAppScaffold(
+      statusBarIconBrightness: Brightness.light,
+      statusBarColor: kcPrimaryColor,
       scaffoldBackgroundColor: kcPrimaryColor,
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text(
-              'STACKED',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
+            SvgPicture.asset(
+              AppAssets.splashLogo,
+              height: 140.h,
+              width: 140.w,
             ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('Loading ...', style: TextStyle(fontSize: 16)),
-                horizontalSpaceSmall,
-                SizedBox(
-                  width: 16,
-                  height: 16,
-                  child: CircularProgressIndicator(
-                    color: Colors.black,
-                    strokeWidth: 6,
-                  ),
-                ),
-              ],
+            UIHelper.verticalSpaceLarge,
+            const TitleTextWidget(
+              text: 'WeightMate',
+              textType: TextType.xxlarge,
+              fontWeight: FontWeight.w500,
             ),
+            UIHelper.verticalSpaceXSmall,
+            const BodyTextWidget(
+              text: 'Weight Price Calculator & Bills',
+              textType: TextType.smedium,
+              color: kcDarkSecondaryText,
+            )
           ],
         ),
       ),
