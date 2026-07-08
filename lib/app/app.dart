69ffe3e12c35af3a0792621094caf54ca64fff50
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-07-08 16:11:48 +0545

    feat: add routes and navigation for CreateBillView, GenerateBillView, and QuickCalculatorView

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,9 @@
 import 'package:weight_mate/ui/bottom_sheets/notice/notice_sheet.dart';
 import 'package:weight_mate/ui/dialogs/info_alert/info_alert_dialog.dart';
 import 'package:weight_mate/ui/views/home/home_view.dart';
+import 'package:weight_mate/ui/views/create_bill/create_bill_view.dart';
+import 'package:weight_mate/ui/views/generate_bill/generate_bill_view.dart';
+import 'package:weight_mate/ui/views/quick_calculator/quick_calculator_view.dart';
 import 'package:weight_mate/ui/views/startup/startup_view.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
@@ -12,6 +15,9 @@ import 'package:weight_mate/ui/views/walkthrough/walkthrough_view.dart';
     MaterialRoute(page: HomeView),
     MaterialRoute(page: StartupView),
     MaterialRoute(page: WalkthroughView),
+    MaterialRoute(page: QuickCalculatorView),
+    MaterialRoute(page: CreateBillView),
+    MaterialRoute(page: GenerateBillView),
 // @stacked-route
   ],
   dependencies: [
--- lib/app/app.router.dart
+++ lib/app/app.router.dart
@@ -6,11 +6,17 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:flutter/material.dart' as _i5;
+import 'package:flutter/material.dart' as _i8;
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart' as _i1;
-import 'package:stacked_services/stacked_services.dart' as _i6;
+import 'package:stacked_services/stacked_services.dart' as _i10;
+import 'package:weight_mate/models/bill.dart' as _i9;
+import 'package:weight_mate/ui/views/create_bill/create_bill_view.dart' as _i6;
+import 'package:weight_mate/ui/views/generate_bill/generate_bill_view.dart'
+    as _i7;
 import 'package:weight_mate/ui/views/home/home_view.dart' as _i2;
+import 'package:weight_mate/ui/views/quick_calculator/quick_calculator_view.dart'
+    as _i5;
 import 'package:weight_mate/ui/views/startup/startup_view.dart' as _i3;
 import 'package:weight_mate/ui/views/walkthrough/walkthrough_view.dart' as _i4;
 
@@ -21,10 +27,19 @@ class Routes {
 
   static const walkthroughView = '/walkthrough-view';
 
+  static const quickCalculatorView = '/quick-calculator-view';
+
+  static const createBillView = '/create-bill-view';
+
+  static const generateBillView = '/generate-bill-view';
+
   static const all = <String>{
     homeView,
     startupView,
     walkthroughView,
+    quickCalculatorView,
+    createBillView,
+    generateBillView,
   };
 }
 
@@ -42,6 +57,18 @@ class StackedRouter extends _i1.RouterBase {
       Routes.walkthroughView,
       page: _i4.WalkthroughView,
     ),
+    _i1.RouteDef(
+      Routes.quickCalculatorView,
+      page: _i5.QuickCalculatorView,
+    ),
+    _i1.RouteDef(
+      Routes.createBillView,
+      page: _i6.CreateBillView,
+    ),
+    _i1.RouteDef(
+      Routes.generateBillView,
+      page: _i7.GenerateBillView,
+    ),
   ];
 
   final _pagesMap = <Type, _i1.StackedRouteFactory>{
@@ -49,7 +76,7 @@ class StackedRouter extends _i1.RouterBase {
       final args = data.getArgs<HomeViewArguments>(
         orElse: () => const HomeViewArguments(),
       );
-      return _i5.MaterialPageRoute<dynamic>(
+      return _i8.MaterialPageRoute<dynamic>(
         builder: (context) => _i2.HomeView(key: args.key),
         settings: data,
       );
@@ -58,7 +85,7 @@ class StackedRouter extends _i1.RouterBase {
       final args = data.getArgs<StartupViewArguments>(
         orElse: () => const StartupViewArguments(),
       );
-      return _i5.MaterialPageRoute<dynamic>(
+      return _i8.MaterialPageRoute<dynamic>(
         builder: (context) => _i3.StartupView(key: args.key),
         settings: data,
       );
@@ -67,8 +94,34 @@ class StackedRouter extends _i1.RouterBase {
       final args = data.getArgs<WalkthroughViewArguments>(
         orElse: () => const WalkthroughViewArguments(),
       );
-      return _i5.MaterialPageRoute<dynamic>(
+      return _i8.MaterialPageRoute<dynamic>(
         builder: (context) => _i4.WalkthroughView(key: args.key),
+        settings: data,
+      );
+    },
+    _i5.QuickCalculatorView: (data) {
+      final args = data.getArgs<QuickCalculatorViewArguments>(
+        orElse: () => const QuickCalculatorViewArguments(),
+      );
+      return _i8.MaterialPageRoute<dynamic>(
+        builder: (context) => _i5.QuickCalculatorView(key: args.key),
+        settings: data,
+      );
+    },
+    _i6.CreateBillView: (data) {
+      final args = data.getArgs<CreateBillViewArguments>(
+        orElse: () => const CreateBillViewArguments(),
+      );
+      return _i8.MaterialPageRoute<dynamic>(
+        builder: (context) => _i6.CreateBillView(key: args.key),
+        settings: data,
+      );
+    },
+    _i7.GenerateBillView: (data) {
+      final args = data.getArgs<GenerateBillViewArguments>(nullOk: false);
+      return _i8.MaterialPageRoute<dynamic>(
+        builder: (context) =>
+            _i7.GenerateBillView(key: args.key, bill: args.bill),
         settings: data,
       );
     },
@@ -84,7 +137,7 @@ class StackedRouter extends _i1.RouterBase {
 class HomeViewArguments {
   const HomeViewArguments({this.key});
 
-  final _i5.Key? key;
+  final _i8.Key? key;
 
   @override
   String toString() {
@@ -106,7 +159,7 @@ class HomeViewArguments {
 class StartupViewArguments {
   const StartupViewArguments({this.key});
 
-  final _i5.Key? key;
+  final _i8.Key? key;
 
   @override
   String toString() {
@@ -128,7 +181,7 @@ class StartupViewArguments {
 class WalkthroughViewArguments {
   const WalkthroughViewArguments({this.key});
 
-  final _i5.Key? key;
+  final _i8.Key? key;
 
   @override
   String toString() {
@@ -147,9 +200,80 @@ class WalkthroughViewArguments {
   }
 }
 
-extension NavigatorStateExtension on _i6.NavigationService {
+class QuickCalculatorViewArguments {
+  const QuickCalculatorViewArguments({this.key});
+
+  final _i8.Key? key;
+
+  @override
+  String toString() {
+    return '{"key": "$key"}';
+  }
+
+  @override
+  bool operator ==(covariant QuickCalculatorViewArguments other) {
+    if (identical(this, other)) return true;
+    return other.key == key;
+  }
+
+  @override
+  int get hashCode {
+    return key.hashCode;
+  }
+}
+
+class CreateBillViewArguments {
+  const CreateBillViewArguments({this.key});
+
+  final _i8.Key? key;
+
+  @override
+  String toString() {
+    return '{"key": "$key"}';
+  }
+
+  @override
+  bool operator ==(covariant CreateBillViewArguments other) {
+    if (identical(this, other)) return true;
+    return other.key == key;
+  }
+
+  @override
+  int get hashCode {
+    return key.hashCode;
+  }
+}
+
+class GenerateBillViewArguments {
+  const GenerateBillViewArguments({
+    this.key,
+    required this.bill,
+  });
+
+  final _i8.Key? key;
+
+  final _i9.Bill bill;
+
+  @override
+  String toString() {
+    return '{"key": "$key", "bill": "$bill"}';
+  }
+
+  @override
+  bool operator ==(covariant GenerateBillViewArguments other) {
+    if (identical(this, other)) return true;
+    return other.key == key && other.bill == bill;
+  }
+
+  @override
+  int get hashCode {
+    return key.hashCode ^ bill.hashCode;
+  }
+}
+
+extension NavigatorStateExtension on _i10.NavigationService {
   Future<dynamic> navigateToHomeView({
-    _i5.Key? key,
+    _i8.Key? key,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
@@ -165,7 +289,7 @@ extension NavigatorStateExtension on _i6.NavigationService {
   }
 
   Future<dynamic> navigateToStartupView({
-    _i5.Key? key,
+    _i8.Key? key,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
@@ -181,7 +305,7 @@ extension NavigatorStateExtension on _i6.NavigationService {
   }
 
   Future<dynamic> navigateToWalkthroughView({
-    _i5.Key? key,
+    _i8.Key? key,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
@@ -196,8 +320,57 @@ extension NavigatorStateExtension on _i6.NavigationService {
         transition: transition);
   }
 
+  Future<dynamic> navigateToQuickCalculatorView({
+    _i8.Key? key,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return navigateTo<dynamic>(Routes.quickCalculatorView,
+        arguments: QuickCalculatorViewArguments(key: key),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> navigateToCreateBillView({
+    _i8.Key? key,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return navigateTo<dynamic>(Routes.createBillView,
+        arguments: CreateBillViewArguments(key: key),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> navigateToGenerateBillView({
+    _i8.Key? key,
+    required _i9.Bill bill,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return navigateTo<dynamic>(Routes.generateBillView,
+        arguments: GenerateBillViewArguments(key: key, bill: bill),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
   Future<dynamic> replaceWithHomeView({
-    _i5.Key? key,
+    _i8.Key? key,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
@@ -213,7 +386,7 @@ extension NavigatorStateExtension on _i6.NavigationService {
   }
 
   Future<dynamic> replaceWithStartupView({
-    _i5.Key? key,
+    _i8.Key? key,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
@@ -229,7 +402,7 @@ extension NavigatorStateExtension on _i6.NavigationService {
   }
 
   Future<dynamic> replaceWithWalkthroughView({
-    _i5.Key? key,
+    _i8.Key? key,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
@@ -238,6 +411,55 @@ extension NavigatorStateExtension on _i6.NavigationService {
   }) async {
     return replaceWith<dynamic>(Routes.walkthroughView,
         arguments: WalkthroughViewArguments(key: key),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithQuickCalculatorView({
+    _i8.Key? key,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return replaceWith<dynamic>(Routes.quickCalculatorView,
+        arguments: QuickCalculatorViewArguments(key: key),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithCreateBillView({
+    _i8.Key? key,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return replaceWith<dynamic>(Routes.createBillView,
+        arguments: CreateBillViewArguments(key: key),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithGenerateBillView({
+    _i8.Key? key,
+    required _i9.Bill bill,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return replaceWith<dynamic>(Routes.generateBillView,
+        arguments: GenerateBillViewArguments(key: key, bill: bill),
         id: routerId,
         preventDuplicates: preventDuplicates,
         parameters: parameters,
