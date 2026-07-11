466d64a8920b45b448502c52480fc1744f1fc238
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-07-11 20:21:42 +0545

    feat: update build.gradle.kts to configure library namespace and clean up manifest package declaration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.LibraryExtension
+
 allprojects {
     repositories {
         google()
@@ -14,8 +16,27 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
+
+    plugins.withId("com.android.library") {
+        extensions.findByType(LibraryExtension::class.java)?.apply {
+            if (namespace == null) {
+                namespace = "com.example.weight_mate"
+            }
+            sourceSets.all {
+                val manifestFile = manifest.srcFile
+                if (manifestFile.exists()) {
+                    var content = manifestFile.readText()
+                    val original = content
+                    content = content.replace(Regex("""\s*package="[^"]*""""), "")
+                    if (content != original) {
+                        manifestFile.writeText(content)
+    
+                    }
+                }
+            }
+        }
+    }
+
     project.evaluationDependsOn(":app")
 }
 
